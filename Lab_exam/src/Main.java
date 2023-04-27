ed59825a7b6e2a6134b2f183fba94acf307aaefc
precupgabrielantoniu <precupgabrielantoniu@yahoo.com>
2023-04-27 18:39:06 +0200

    Added product 5648578

--- Lab_exam/src/Main.java
+++ Lab_exam/src/Main.java
@@ -16,10 +16,14 @@ public class Main {
         Product p3 = new Product();
         p3.setName("carne de vita");
         p3.setPrice(55.);
-        p3.setId(5648577L);
+        p3.setId(5645577L);
         Product p4 = new Product();
         p4.setPrice(6.8);
-        p4.setId(5648578L);
+        p4.setId(5448578L);
+        Product p5 = new Product();
+        p5.setName("Doctor Marius");
+        p5.setPrice(6.8);
+        p5.setId(5648578L);
         Offer o1 = new Offer();
         o1.setCustomer("Angela Dumitru");
         ProductRepository productRepository = new ProductRepository();
@@ -32,6 +36,7 @@ public class Main {
         ProductRepository productRepository1 = new ProductRepository();
         ProductService ps2 = new ProductService(productRepository1);
         ps2.create(p3);
+        ps2.create(p5);
         System.out.println(ps.search("oU"));
     }
 }
